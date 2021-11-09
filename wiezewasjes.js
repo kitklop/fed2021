ef8d4d0b8d15eba54668f4a6a529068a3aa6ccac
kitklop <43644853+kitklop@users.noreply.github.com>
2021-11-09 14:17:01 +0100

    Add files via upload

---
+++ wiezewasjes.js
@@ -0,0 +1,14 @@
+var header = document.querySelector('header');
+//leeg wagentje
+
+var toevoegen = document.querySelector('button.voegToe'); 
+//voeg toe aan winkelmandje
+
+
+toevoegen.addEventListener('click', winkelwagentje);
+
+
+function winkelwagentje(event){
+    event.preventDefault();
+    header.classList.toggle('add');
+}
